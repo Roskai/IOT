8c6c5a506c26da41856fbdbb22357f813497e886
Roskai <93260994+Roskai@users.noreply.github.com>
2023-02-21 17:41:10 +0100

    singleton

--- src/main/java/entities/Environement.java
+++ src/main/java/entities/Environement.java
@@ -3,6 +3,7 @@ package entities;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import javax.inject.Singleton;
 import java.time.LocalTime;
 
 @Data
@@ -17,8 +18,18 @@ import java.time.LocalTime;
  * @author roskai
  *
  */
-
+@Singleton
 public class Environement {
+    private static Environement INSTANCE;
+    
     private int brightness;
     private LocalTime timeOfDay;
+    
+    public static Environement getInstance() {
+        if(INSTANCE == null) {
+            INSTANCE = new Environement();
+        }
+
+        return INSTANCE;
+    }
 }
